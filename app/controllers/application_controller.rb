657c74a00b8c3a5ce8e4e14239c59eccd1e93072
tch399 <tch399@gmail.com>
2020-03-04 12:31:10 -0500

    changes controller to add spanish method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
-	def hello
+	def hola
 		render html: "¡Hola, mundo!"
 	end
 
 	def goodbye
 		render html: "goodbye, world"
+	end
+
+	def hello
+		render html: "Hello!"
 	end
 end
